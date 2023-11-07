e4136f790d858ba1ffce2ce8e7cd3da371ebcd7e
Diego Cuevas <ingdcuevas@gmail.com>
2023-11-07 10:30:31 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Blog de sexualidad de juli
+      </h1>
+    </>
+  );
+}
+
+export default App;
