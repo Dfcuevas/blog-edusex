31ac6dcc6bea75b2a7841ae0809af18b69271087
Diego Cuevas <ingdcuevas@gmail.com>
2023-11-08 16:22:22 -0500

    Terminado el componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,31 @@
+import Logo from "../../public/logo.png";
+
+const arrayDatosNav = ["Soy maestro", "Soy estudiante", "Soy padre"];
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-4 px-12 flex justify-between items-center">
+      <div className="flex items-center gap-4">
+        <img
+          className="rotate-90"
+          src={Logo}
+          alt="Logo del blog"
+          width={30}
+          height={30}
+        />
+        <span className="text-2xl">Hablemos de Sexualidad</span>
+      </div>
+      <nav>
+        <ul className="flex gap-3 text-2xl">
+          {arrayDatosNav.map((item, index) => (
+            <li className="text-purpuraOscuro" key={index}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
